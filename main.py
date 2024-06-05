b4b220dcda8ed5564303f2e4a313e46649943e8b
Luis Hernandez <luisrnandezc@gmail.com>
2024-06-05 12:06:27 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# """
+# NAV Aviation Academy.
+# Project 1: Flight Management System
+# Author: Luis Hernández
+# GitHub: luisrnandezc
+# Date: 04/06/2024
+# """
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    main()

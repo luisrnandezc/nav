38af28280978df669d672ea988b2cc2350267ca4
Luis Hernandez <luisrnandezc@gmail.com>
2024-07-12 20:08:38 -0400

    Update index page of home_app

--- config/config/settings.py
+++ config/config/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home_app',
+    'schedule_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

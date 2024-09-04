22065c3258cdf5284adca2006ceb64200faf38bd
Luis Hernandez <luisrnandezc@gmail.com>
2024-09-04 15:08:03 -0400

    feat: add initial javascript for slides entrance effect

--- config/home_app/static/dynamics.js
+++ config/home_app/static/dynamics.js
@@ -25,3 +25,25 @@ window.addEventListener('scroll', () => {
     
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
 });
+
+// Sideways entrance effecr for the course page.
+document.addEventListener('scroll', function() {
+    const slides = document.querySelectorAll('.course-slide');
+    const windowHeight = window.innerHeight;
+    const centerScreen = windowHeight / 2;
+
+    slides.forEach(slide => {
+      const rect = slide.getBoundingClientRect();
+      const slideCenter = rect.top + rect.height / 2;
+      const distanceFromCenter = Math.abs(centerScreen - slideCenter);
+      
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        slide.classList.add('visible');
+        const opacity = Math.max(0, 1 - distanceFromCenter / centerScreen);
+        slide.style.opacity = opacity;
+      } else {
+        slide.classList.remove('visible');
+        slide.style.opacity = 1;
+      }
+    });
+});

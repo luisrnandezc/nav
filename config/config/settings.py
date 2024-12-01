5add52088fdb439a2507e635de0846569b47ad71
Luis Hernandez <luisrnandezc@gmail.com>
2024-11-30 20:55:38 -0400

    refactor: move accounts app to the top of INSTALLED_APPS list

--- config/config/settings.py
+++ config/config/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'home_app',
     'accounts',
+    'home_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

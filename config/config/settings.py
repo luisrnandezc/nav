ee149aa89b0f207bcccd7eaa8e4297b836c16937
Luis Hernandez <luisrnandezc@gmail.com>
2024-12-04 04:57:28 -0400

    feat: change app name from home_app to website and add dashboard app

--- config/config/settings.py
+++ config/config/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
-    'home_app',
+    'website',
+    'dashboard',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

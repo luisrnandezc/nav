5a692c198f389b8e8bcb9491f31edd04e88094ab
Luis Hernandez <luisrnandezc@gmail.com>
2024-12-06 21:22:09 -0400

    style: add customized styles to the login view.

--- config/accounts/forms.py
+++ config/accounts/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-field"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-field"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-field', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-field', 'placeholder': 'Password'}))

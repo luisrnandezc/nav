7963447f0147fbcc66d3e7934fe9b0fad5f1ee7d
Luis Hernandez <luisrnandezc@gmail.com>
2024-12-07 02:18:38 -0400

    style: update the style of the password_change_done webpage

--- config/accounts/forms.py
+++ config/accounts/forms.py
@@ -1,6 +1,28 @@
 from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-field', 'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-field', 'placeholder': 'Password'}))
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label='Contraseña actual',
+        widget=forms.PasswordInput(attrs={
+            'class': 'password-field',
+        })
+    )
+    new_password1 = forms.CharField(
+        label='Nueva contraseña',
+        widget=forms.PasswordInput(attrs={
+            'class': 'password-field',
+        })
+    )
+    new_password2 = forms.CharField(
+        label='Nueva contraseña',
+        widget=forms.PasswordInput(attrs={
+            'class': 'password-field',
+        })
+    )

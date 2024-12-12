37b2c9589c3738c51610e58873d057c554577eef
Luis Hernandez <luisrnandezc@gmail.com>
2024-12-12 15:55:49 -0400

    feat: add custom user profiles to display properties in html files.

--- config/dashboard/views.py
+++ config/dashboard/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from accounts.models import Student, Instructor, Staff
 
 @login_required
 def student_dashboard(request):
     user = request.user
-    context = {
-        'username': user.username,
-        'email': user.email,
-        'first_name': user.first_name,
-        'last_name': user.last_name,
-    }
-    return render(request, "dashboard/student_dashboard.html", context)
+    student_profile = None
+    instructor_profile = None
+    staff_profile = None
+
+    # Check if the user is a student
+    try:
+        student_profile = Student.objects.get(user=user)
+        context = {
+            'user': user,
+            'user_profile': student_profile,
+        }
+        return render(request, "dashboard/student_dashboard.html", context)
+    except Student.DoesNotExist:
+        pass
+
+    # Check if the user is an instructor
+    try:
+        instructor_profile = Instructor.objects.get(user=user)
+        context = {
+            'user': user,
+            'user_profile': instructor_profile,
+        }
+        return render(request, "dashboard/instructor_dashboard.html", context)
+    except Instructor.DoesNotExist:
+        pass
+
+    # Check if the user is staff
+    try:
+        staff_profile = Staff.objects.get(user=user)
+        context = {
+            'user': user,
+            'user_profile': staff_profile,
+        }
+        return render(request, "dashboard/staff_dashboard.html", context)
+    except Staff.DoesNotExist:
+        pass
+
+    return render(request, "dashboard/error_dashboard.html", context)

f845c27a77854656a6f8d1952c7bd9b2e2254598
Luis Hernandez <luisrnandezc@gmail.com>
2025-02-13 14:15:52 -0400

    feat: 3d model fully functional

--- config/website/static/js/3dviewer.js
+++ config/website/static/js/3dviewer.js
@@ -1,14 +1,25 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // Scene creation
 const container = document.getElementById('viewer-container');
 const canvas = document.getElementById('3d-viewer');
 
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true }); // alpha = true for transparent background.
+renderer.setClearColor(0x000000, 0);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, container.clientWidth / container.clientHeight, 0.1, 1000 );
-camera.position.z = 5;
+camera.position.z = 7;
+
+const light = new THREE.AmbientLight(0xffffff, 5);
+scene.add(light);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.screenSpacePanning = false;
+controls.maxPolarAngle = Math.PI / 2;
 
 resizeRendererToContainer(); // Set initial size
 
@@ -39,7 +50,7 @@ window.addEventListener('resize', resizeRendererToContainer);
 
 function animate() {
     if (model) { // Check if model is loaded before rotating
-        model.rotation.y += 0.01;
+        model.rotation.y += 0.001;
     }
 	renderer.render( scene, camera );
 }

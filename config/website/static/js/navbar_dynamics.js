ae134e7dfaaae50bfc39b887557f7f16b42b18ce
Luis Hernandez <luisrnandezc@gmail.com>
2025-03-20 13:16:34 -0400

    bugfix: navbar not hidding when scrolling up on the courses page. The problem was a overflow-y: scroll property that was targeting all the elements in the html

--- config/website/static/js/navbar_dynamics.js
+++ config/website/static/js/navbar_dynamics.js
@@ -1,25 +1,28 @@
-// navbar burger animation
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
-})
-
-// navbar scrolling-hide effect.
 document.addEventListener('DOMContentLoaded', () => {
-    let navbarLastScrollY = window.scrollY;
+    // navbar burger animation
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
     const navbar = document.querySelector('.navbar');
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > navbarLastScrollY) {
-            // Scrolling down, hide navbar
-            navbar.classList.add('navbar-hidden');
-        } else {
-            // Scrolling up, show navbar
-            navbar.classList.remove('navbar-hidden');
-        }
-        navbarLastScrollY = window.scrollY;
-    });
+    if (burger && navLinks) {
+        burger.addEventListener('click', () => {
+            navLinks.classList.toggle('nav-active');
+            burger.classList.toggle('toggle');
+        });
+    }
+
+    // navbar scrolling-hide effect.
+    if (navbar) {
+        let navbarLastScrollY = window.scrollY;
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > navbarLastScrollY) {
+                // Scrolling down, hide navbar
+                navbar.classList.add('navbar-hidden');
+            } else {
+                // Scrolling up, show navbar
+                navbar.classList.remove('navbar-hidden');
+            }
+            navbarLastScrollY = window.scrollY;
+        });
+    }
 });

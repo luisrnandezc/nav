a128d079dbd20c1cbd6ec19ad2f859a6b3021832
Luis Hernandez <luisrnandezc@gmail.com>
2025-04-02 18:11:28 -0400

    misc: deleted redundant import

--- config/accounts/models.py
+++ config/accounts/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError

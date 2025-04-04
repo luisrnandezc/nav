4b4c45133f598d09f4e3b6f7d455c8bd16b83cfb
Luis Hernandez <luisrnandezc@gmail.com>
2025-04-04 14:12:52 -0400

    feature: academic and accounts models created and working properly

--- config/accounts/admin.py
+++ config/accounts/admin.py
@@ -21,7 +21,37 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ('username', 'email', 'national_id', 'first_name', 'last_name')
     ordering = ('username',)
 
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'student_phase', 'get_course_type', 'get_course_edition')
+    list_filter = ('student_phase',)
+    search_fields = ('user__username', 'user__national_id', 'user__first_name', 'user__last_name')
+    readonly_fields = ('get_course_type', 'get_course_edition')
+    
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user',)
+        }),
+        ('Student Information', {
+            'fields': ('student_age', 'student_gender', 'student_phase', 'student_license_type')
+        }),
+        ('Course Information', {
+            'fields': ('get_course_type', 'get_course_edition'),
+            'description': 'Esta información se actualiza automáticamente según la inscripción del estudiante en los cursos.'
+        }),
+        ('Balance Information', {
+            'fields': ('student_balance',)
+        }),
+    )
+
+    def get_course_type(self, obj):
+        return obj.current_course_type
+    get_course_type.short_description = 'Curso'
+
+    def get_course_edition(self, obj):
+        return obj.current_course_edition
+    get_course_edition.short_description = 'Edición de Curso'
+
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(StudentProfile)
 admin.site.register(InstructorProfile)
 admin.site.register(StaffProfile)

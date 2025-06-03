68cca2c695855e755c5699cb95a43a5f30b4ad59
luisrnandezc <luisrnandezc@gmail.com>
2025-06-03 14:23:11 -0400

    feat: simplify the subject edition and student fields for the grades admin inside the academic app

--- config/academic/admin.py
+++ config/academic/admin.py
@@ -22,13 +22,30 @@ class SubjectTypeAdmin(admin.ModelAdmin):
 
 @admin.register(SubjectEdition)
 class SubjectEditionAdmin(admin.ModelAdmin):
-    list_display = ('subject_type', 'instructor', 'time_slot', 'start_date', 'end_date')
+    list_display = ('get_subject_code', 'instructor', 'time_slot', 'start_date', 'end_date')
     list_filter = ('subject_type__course_type', 'time_slot')
     search_fields = ('subject_type__name', 'subject_type__code', 'instructor__username')
     filter_horizontal = ('students',)
 
+    def get_subject_code(self, obj):
+        return obj.subject_type.code if obj.subject_type else '-'
+    get_subject_code.short_description = 'Materia'
+    get_subject_code.admin_order_field = 'subject_type__code'
+
 @admin.register(StudentGrade)
 class StudentGradeAdmin(admin.ModelAdmin):
-    list_display = ('subject_edition', 'student', 'grade', 'test_type')
+    list_display = ('get_subject_info', 'get_student_username', 'grade', 'test_type')
     list_filter = ('subject_edition__subject_type__course_type', 'test_type')
     search_fields = ('subject_edition__subject_type__name', 'subject_edition__subject_type__code', 'student__username')
+
+    def get_subject_info(self, obj):
+        if obj.subject_edition and obj.subject_edition.subject_type:
+            return f"{obj.subject_edition.subject_type.code} ({obj.subject_edition.time_slot})"
+        return '-'
+    get_subject_info.short_description = 'Materia'
+    get_subject_info.admin_order_field = 'subject_edition__subject_type__code'
+
+    def get_student_username(self, obj):
+        return obj.student.username if obj.student else '-'
+    get_student_username.short_description = 'Estudiante'
+    get_student_username.admin_order_field = 'student__username'

dc6a298a4ae45655cdfd867506ec8d7472a2169c
luisrnandezc <luisrnandezc@gmail.com>
2025-06-17 14:41:25 -0400

    feat: fix url patterns and improve form selection buttons styling

--- config/fms/urls.py
+++ config/fms/urls.py
@@ -5,5 +5,5 @@ app_name = 'fms'
 
 urlpatterns = [
     path('fms/form_selection/', views.form_selection, name='form_selection'),
-    path('fms/submit_flight_evaluation/', views.submit_flight_evaluation, name='flight_evaluation'),
+    path('fms/submit_flight_evaluation/', views.submit_flight_evaluation, name='flight_evaluation_0_100'),
 ]

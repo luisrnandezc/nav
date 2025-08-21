aac8ab688dda42ae060a7e8684452df477eee332
luisrnandezc <luisrnandezc@gmail.com>
2025-08-21 12:24:35 -0400

    feat: add error handling to the payment email alert and add minor improvement for production

--- config/payments/signals.py
+++ config/payments/signals.py
@@ -3,6 +3,17 @@ from django.dispatch import receiver
 from .models import StudentPayment
 from django.core.mail import send_mail
 from django.conf import settings
+import logging
+
+logger = logging.getLogger('payments.signals')
+
+PAYMENT_NOTIFICATION_SUBJECT = """Nuevo pago agregado"""
+PAYMENT_NOTIFICATION_MESSAGE = """
+Se ha agregado un nuevo pago de ${amount} a la cuenta de {student_name}.
+Tipo de pago: {payment_type}
+Fecha de pago: {date_added}
+Estado: Pendiente de confirmación
+"""
 
 @receiver(post_save, sender=StudentPayment)
 def send_payment_confirmation_email(sender, instance, created, **kwargs):
@@ -10,11 +21,20 @@ def send_payment_confirmation_email(sender, instance, created, **kwargs):
     Signal handler to send payment confirmation email when payment is saved.
     """
     if created and instance.confirmed is False:
-        # Send email to student
-        send_mail(
-            'Nuevo pago agregado',
-            f'Se ha agregado un nuevo pago de ${instance.amount} a la cuenta de {instance.student_profile.user.username}.',
-            settings.EMAIL_HOST_USER,
-            [settings.RECEIVER_EMAIL],
-            fail_silently=False,
-        )
+        try:
+            # Send email to the school director
+            send_mail(
+                PAYMENT_NOTIFICATION_SUBJECT,
+                PAYMENT_NOTIFICATION_MESSAGE.format(
+                    amount=instance.amount,
+                    payment_type=instance.type,
+                    student_name=instance.student_profile.user.get_full_name(),
+                    date_added=instance.date_added,
+                ),
+                settings.DEFAULT_FROM_EMAIL,
+                [settings.RECEIVER_EMAIL],
+                fail_silently=False,
+            )
+            logger.info(f"Email sent to {settings.RECEIVER_EMAIL} with subject 'Nuevo pago agregado'")
+        except Exception as e:
+            logger.error(f"Failed to send payment confirmation email to {settings.RECEIVER_EMAIL}: {e}")

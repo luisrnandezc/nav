42c1b13eaea67e060680031a4afb649964b35e60
luisrnandezc <luisrnandezc@gmail.com>
2025-08-22 12:22:10 -0400

    debug: delete all the debug files and statements

--- config/sms/urls.py
+++ config/sms/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from .debug_email import email_debug_view
 
 app_name = 'sms'
 
 urlpatterns = [
     # Define your URL patterns here
     # path('', views.index, name='index'),
-    path('email-debug/', email_debug_view, name='email_debug'),
 ]

819ab9a5152f96d5634cf1e1fae33ef14cab9119
luisrnandezc <luisrnandezc@gmail.com>
2025-08-29 15:05:20 -0400

    feat: create main sms page to concentrate all the safety related features

--- config/sms/urls.py
+++ config/sms/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'sms'
 
 urlpatterns = [
+    path('', views.main_sms, name='main_sms'),
     path('voluntary_report/', views.voluntary_report, name='voluntary_report'),
 ]

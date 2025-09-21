29dad7d31396252398c9b010334f8e9c735f8407
luisrnandezc <luisrnandezc@gmail.com>
2025-09-21 15:00:25 -0400

    refactor: minor modifications

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&^7+-ow54x%!aopkqw@lfd59h=b+b&%9mz=s1df1@n6^1y=jht'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Check if we're on PythonAnywhere (production)

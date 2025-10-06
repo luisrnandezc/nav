9206414bbc4c2d626f27ef44121388c4075ae16c
luisrnandezc <luisrnandezc@gmail.com>
2025-10-06 18:01:50 -0400

    feat: create instructor grades dashboard

--- config/academic/urls.py
+++ config/academic/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('submit-grade/', views.submit_student_grade, name='submit_grade'),
     path('ajax/load-students/', views.load_students, name='ajax_load_students'),
     path('grade_logs/', views.grade_logs, name='grade_logs'),
+    path('instructor-grades-dashboard/', views.instructor_grades_dashboard, name='instructor_grades_dashboard'),
 ]

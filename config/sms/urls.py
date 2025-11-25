664432a2b84d3bec639b5756491ce616f4a6651b
luisrnandezc <luisrnandezc@gmail.com>
2025-11-25 15:43:22 -0400

    feat: add form to update severity and probability for each individual risk

--- config/sms/urls.py
+++ config/sms/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('voluntary_hazard_report/', views.voluntary_hazard_report, name='voluntary_hazard_report'),
     path('voluntary_hazard_report/<int:report_id>/', views.voluntary_hazard_report_detail, name='voluntary_hazard_report_detail'),
     path('voluntary_hazard_report/<int:report_id>/risk/<str:risk_key>/delete/', views.delete_risk, name='delete_risk'),
+    path('voluntary_hazard_report/<int:report_id>/risk/<str:risk_key>/evaluation/update/', views.update_risk_evaluation, name='update_risk_evaluation'),
     path('voluntary_hazard_report/<int:report_id>/risk/<str:risk_key>/actions/<int:action_index>/delete/', views.delete_action, name='delete_action'),
     path('voluntary_hazard_report/<int:report_id>/risk/add/', views.add_risk, name='add_risk'),
     path('voluntary_hazard_report/<int:report_id>/risk/<str:risk_key>/actions/add/', views.add_action, name='add_action'),

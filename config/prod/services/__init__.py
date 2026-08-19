979e7959b19314ea3401e22711368e001e0f0573
luisrnandezc <luisrnandezc@gmail.com>
2026-08-19 16:27:13 -0400

    feat: implement service for production calculations and corresponding tests

--- config/prod/services/__init__.py
+++ config/prod/services/__init__.py
@@ -1,3 +1,18 @@
 from .fuel import FuelConsumptionReport, get_fuel_consumed
+from .production import (
+    ProductionBreakdown,
+    ProductionFilters,
+    ProductionReport,
+    ProductionTotals,
+    get_production_report,
+)
 
-__all__ = ['FuelConsumptionReport', 'get_fuel_consumed']
+__all__ = [
+    'FuelConsumptionReport',
+    'ProductionBreakdown',
+    'ProductionFilters',
+    'ProductionReport',
+    'ProductionTotals',
+    'get_fuel_consumed',
+    'get_production_report',
+]
